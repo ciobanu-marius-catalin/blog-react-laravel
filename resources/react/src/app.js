19981a2c9019d7539cd7ae47f84e7421f72b2cd8
Ciobanu Catalin <ciobanu.m.catalin.pfa@gmail.com>
2021-11-14 14:27:39 +0200

    made initial configuration to use react

---
+++ resources/react/src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+
+const App = () => {
+    let test = 4;
+
+    let name = `${test}`;
+    return (
+        <h1>
+            <h2>React app</h2>
+        </h1>
+    );
+};
+
+export { App };
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+
+module.exports = (env) => {
+    return {
+        entry: {
+            app: {
+                import: "./resources/react/src/index.js",
+                filename: "js/app.min.js"
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"]
+                },
+                {
+                    test: /\.(s(a|c)ss)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                "@": resolve("resources/js/src")
+            },
+            extensions: ["*", ".js", ".jsx"]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({ parallel: true, extractComments: false })
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "css/[name].min.css"
+            })
+        ],
+        output: {
+            path: path.resolve(__dirname, "./public"),
+            filename: "[name].min.js"
+        }
+    };
+};
