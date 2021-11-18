595f17890e1c3e457c49b627d62bc02490ff1add
Ciobanu Catalin <ciobanu.m.catalin.pfa@gmail.com>
2021-11-18 23:41:08 +0200

    minor update to the login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,6 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -50,7 +49,15 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <div class="col-md-8 offset-md-4 text-left">
+                                {{ __('Log in or') }}
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    {{ __('Create Account') }}
+                                </a>
 
+                            </div>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
