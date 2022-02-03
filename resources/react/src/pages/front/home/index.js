e4eb5e77805d42cfc393a880f1f97341abca3097
Catalin Ciobanu <ciobanu.m.catalin.pfa@gmaill.com>
2022-02-03 23:32:28 +0200

    updated the front page github link

--- resources/react/src/pages/front/home/index.js
+++ resources/react/src/pages/front/home/index.js
@@ -7,10 +7,15 @@ const HomePage = () => {
             <div className="gravity-front-page__hero">
                 <h1 className="gravity-front-page__title">Blog</h1>
                 <p className="gravity-front-page__description">
-                    This blog was created by Ciobanu Marius-Catalin for the
-                    github portfolio using React and Laravel as the main
-                    technologies. You can view the posts or log in to view the
-                    CMS dashboard
+                    <span>This blog was created by Ciobanu Marius-Catalin for my</span>
+                    &nbsp;
+                    <a href="https://github.com/ciobanu-marius-catalin/blog-react-laravel" target="_blank">
+                       Github portfolio
+                    </a>
+                    &nbsp;
+                    <span>to show a sample of my coding using React and Laravel as the main technologies. You can
+                    view the posts on this page or log in to view the CMS dashboard
+                    </span>
                 </p>
                 <div className="gravity-front-page__search">
                     <SearchInput placeholder="Search for posts" />
